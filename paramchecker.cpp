5b72e13227558fc411d8776dc1c66ae4edb764e4
PrakashBosch <68539031+PrakashBosch@users.noreply.github.com>
2020-07-23 11:22:07 +0530

    OCP - refactoring into Structure

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,10 +1,16 @@
 enum vital
 {
-  bpm=0,
-  resprate,
-  spo2,
+  ebpm=0,
+  eresprate,
+  espo2,
   n
 };
+typedef struct 
+    {
+	float bpm;
+	float resprate;
+	float spo2;
+	}Params;
 
 bool vital_check(float arg_param, int MIN, int MAX);
 bool vital_check(float arg_param, int MIN, int MAX)
@@ -12,46 +18,47 @@ bool vital_check(float arg_param, int MIN, int MAX)
   return (arg_param < MIN || arg_param > MAX);
 }
 
-bool vital_checkbpm (float arg_bpm);
-bool vital_checkbpm (float arg_bpm)
+bool Vital_IsBpmInRange (Params arg_bpm);
+bool Vital_IsBpmInRange (Params arg_bpm)
 {
-  return vital_check(arg_bpm,70,150);
+  return vital_check(arg_bpm.bpm,70,150);
 }
 
-bool vital_checkrespRate (float arg_respRate);
-bool vital_checkrespRate (float arg_respRate)
+bool Vital_IsResprateInRange (Params arg_respRate);
+bool Vital_IsResprateInRange (Params arg_respRate)
 {
-  return vital_check(arg_respRate,30,60);
+  return vital_check(arg_respRate.resprate,30,60);
 }
 
-bool vital_checkspo2 (float arg_spo2);
-bool vital_checkspo2 (float arg_spo2)
+bool Vital_IsSpo2InRange (Params arg_spo2);
+bool Vital_IsSpo2InRange (Params arg_spo2)
 {
-  return vital_check(arg_spo2,80,100);
+  return vital_check(arg_spo2.spo2,80,100);
 }
 
-bool (*vital_param[n])(float param);
+typedef bool (*check)(Params param);
+check vital_param[n];
 
-void vital_param_resgister(int ID, bool addr);
-void vital_param_resgister(ID, bool addr)
+void vital_param_resgister(int ID, check addr);
+void vital_param_resgister(int ID, check addr)
 {
-   vital_param[ID] = &addr;
+   vital_param[ID] = addr;
 }
 
 void registration(void);
 void registration(void)
 {
-   vital_param_resgister(bpm,&vital_checkbpm);
-   vital_param_resgister(resprate,&vital_checkrespRate);
-   vital_param_resgister(spo2,&vital_checkspo2);
+   vital_param_resgister(ebpm,&Vital_IsBpmInRange);
+   vital_param_resgister(eresprate,&Vital_IsResprateInRange);
+   vital_param_resgister(espo2,&Vital_IsSpo2InRange);
 }
 
-bool vitalsAreOk(float *vitals) {
+bool vitalsAreOk(Params vitals) {
   
   bool ret_status = 0;
   
   for (int i =0; i<n;i++)
-    ret_status = ret_status || vital_param[i](vitals[i]);
+    ret_status = ret_status || vital_param[i](vitals);
   
   return !(ret_status);
 }
